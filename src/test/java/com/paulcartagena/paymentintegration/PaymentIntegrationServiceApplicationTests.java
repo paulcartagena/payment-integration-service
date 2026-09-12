e937b3a044c8dd9c1dc70f3b78fa508f54cdc691
paulcartagena <paulcartagena04@gmail.com>
2026-09-12 15:00:54 -0600

    scaffolding con Spring Boot 4.1, JPA, H2, Security, OpenAPI

---
+++ src/test/java/com/paulcartagena/paymentintegration/PaymentIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulcartagena.paymentintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
