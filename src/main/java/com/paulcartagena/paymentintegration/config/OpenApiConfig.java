6266b5186c07a2a42d10f1f559fa0e62598c464e
paulcartagena <paulcartagena04@gmail.com>
2026-09-12 16:46:09 -0600

    docs: configuración de OpenAPI con esquema de autenticación Basic

---
+++ src/main/java/com/paulcartagena/paymentintegration/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.paulcartagena.paymentintegration.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Payment Integration Service",
+                version = "1.0",
+                description = "Recibe pagos en JSON desde el canal digital, los transforma al formato XML del sistema core y los envía."
+        ),
+        security = @SecurityRequirement(name = "basicAuth")
+)
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+public class OpenApiConfig {
+}
